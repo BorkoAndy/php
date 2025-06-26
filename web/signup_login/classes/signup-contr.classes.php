dddaf764e897c695145da7d15ed9e7a225a79f9c
BorkoAndrii <1983andy2016@gmail.com>
2025-06-26 10:47:19 +0200

    form validation

--- web/signup_login/classes/signup-contr.classes.php
+++ web/signup_login/classes/signup-contr.classes.php
@@ -19,7 +19,30 @@ class SignupContr {
     //Validation of fields, checking pwd and repeatpwd etc
 
     private function emptyInput(){
-        
+        if(empty($this->uid) || empty($this->pwd) || empty($this->pwdrepeat) || empty($this->email)){
+            return false;        
+        }
+        return true;
     }
 
+    private function invalidUid(){
+        if(!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)){
+            return false;        
+        }
+        return true;
+    }
+
+    private function invalidEmail(){
+        if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            return false;        
+        }
+        return true;
+    }
+    
+    private function pwdMatch(){
+        if($this->pwd !== $this->pwdrepeat){
+            return false;        
+        }
+        return true;
+    }
 }
